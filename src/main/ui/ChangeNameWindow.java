03f75acb050894ffcc1e073a95ffcf5c8c517ee2
wmostrenko <wmostrenko@gmail.com>
2024-11-19 18:54:29 -0800

    Refactored change time step window

--- src/main/ui/ChangeNameWindow.java
+++ src/main/ui/ChangeNameWindow.java
@@ -1,18 +1,14 @@
 package ui;
 
 import model.Simulation;
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
+
 import javax.swing.*;
 
-import java.awt.event.ActionListener;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class ChangeNameWindow extends TextAndButtonWindow {
-
-public ChangeNameWindow(Simulation simulation) {
+    
+    public ChangeNameWindow(Simulation simulation) {
         super("Change Name", 300, 100, simulation);
     }
 
--- src/main/ui/ChangeTimeStepWindow.java
+++ src/main/ui/ChangeTimeStepWindow.java
@@ -1,80 +1,31 @@
 package ui;
 
 import model.Simulation;
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
+
 import javax.swing.*;
 
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public class ChangeTimeStepWindow {
-    private JFrame frame;
-    private JPanel mainPanel;
-    private Simulation simulation;
-    JTextField textField;
-
-    private static int WHITESPACE = 7;
+public class ChangeTimeStepWindow extends TextAndButtonWindow {
 
     public ChangeTimeStepWindow(Simulation simulation) {
-        this.simulation = simulation;
-        initializeFrame();
+        super("Change Time Step", 300, 100, simulation);
     }
 
-    public void initializeFrame() {
-        frame = new JFrame();
-        this.frame.setTitle("Change Time Step");
-        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setSize(300, 100);
-        this.frame.setLocationRelativeTo(null);
-        this.frame.setResizable(false);
-        initializeMainPanel();
-        this.frame.setVisible(true);
+    @Override
+    protected void initializeLabels() {
+        labels = new ArrayList<JLabel>();
+        labels.add(new JLabel("Time Step: "));
     }
 
-    private void initializeMainPanel() {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(0, 1, 10, 0));
-        mainPanel.setBackground(Color.WHITE);
-        initializeFieldPanels();
-        initializeChangeTimeStepButton();
-        mainPanel.setBorder(BorderFactory.createMatteBorder(WHITESPACE, WHITESPACE, WHITESPACE, WHITESPACE, Color.WHITE));
-
-        frame.add(mainPanel);
+    @Override
+    protected void initializeTextFields() {
+        textFields = new ArrayList<JTextField>();
+        textFields.add(new JTextField(Double.toString(simulation.getTimeStep())));
     }
 
-    private void initializeFieldPanels() {
-        mainPanel.add(createPanel());
-    }
-
-    private JPanel createPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 2));
-        panel.setBackground(Color.WHITE);
-
-        JLabel label = new JLabel("Timestep: ");
-        panel.add(label);
-
-        textField = new JTextField(Double.toString(simulation.getTimeStep()));
-        panel.add(textField);
-        
-        return panel;
-    }
-
-    private void initializeChangeTimeStepButton() {
-        JButton changeTimeStepButton = createChangeTimeStepButton();
-        mainPanel.add(changeTimeStepButton);
-    }
-
-    private JButton createChangeTimeStepButton() {
-        JButton button = new JButton("Change Time Step");
-        button.setFocusable(false);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                simulation.changeTimeStep(Double.parseDouble(textField.getText()));
-            }
-        });
-        return button;
+    @Override
+    protected void buttonAction() {
+        simulation.changeTimeStep(Double.parseDouble(textFields.get(0).getText()));
     }
 }
