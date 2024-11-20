4aa120118c3a67ac395d9655dc8ed43e4ef3acbb
wmostrenko <wmostrenko@gmail.com>
2024-11-19 19:00:59 -0800

    Refactored load simulation window

--- src/main/ui/LoadSimulationWindow.java
+++ src/main/ui/LoadSimulationWindow.java
@@ -3,86 +3,36 @@ package ui;
 import model.Simulation;
 import persistence.JsonReader;
 
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
 import javax.swing.*;
 
-import java.awt.event.ActionListener;
 import java.io.IOException;
+import java.util.ArrayList;
 
-public class LoadSimulationWindow {
-    private JFrame frame;
-    private JPanel mainPanel;
-    private Simulation simulation;
-    private JTextField textField;
-
-    private static int WHITESPACE = 7;
+public class LoadSimulationWindow extends TextAndButtonWindow {
 
     public LoadSimulationWindow(Simulation simulation) {
-        this.simulation = simulation;
-        initializeFrame();
+        super("Load Simulation", 300, 100, simulation);
     }
 
-    public void initializeFrame() {
-        frame = new JFrame();
-        this.frame.setTitle("Load Simulation");
-        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setSize(300, 100);
-        this.frame.setLocationRelativeTo(null);
-        this.frame.setResizable(false);
-        initializeMainPanel();
-        this.frame.setVisible(true);
+    @Override
+    protected void initializeLabels() {
+        labels = new ArrayList<JLabel>();
+        labels.add(new JLabel("Simulation Name: "));
     }
 
-    private void initializeMainPanel() {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(0, 1, 10, 0));
-        mainPanel.setBackground(Color.WHITE);
-        initializeFieldPanels();
-        initializeSimulationNameButton();
-        mainPanel.setBorder(BorderFactory.createMatteBorder(WHITESPACE, WHITESPACE, WHITESPACE, WHITESPACE, Color.WHITE));
-
-        frame.add(mainPanel);
+    @Override
+    protected void initializeTextFields() {
+        textFields = new ArrayList<JTextField>();
+        textFields.add(new JTextField(simulation.getName()));
     }
 
-    private void initializeFieldPanels() {
-        mainPanel.add(createPanel());
-    }
-
-    private JPanel createPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 2));
-        panel.setBackground(Color.WHITE);
-
-        JLabel label = new JLabel("Simulation Name: ");
-        panel.add(label);
-
-        textField = new JTextField(simulation.getName());
-        panel.add(textField);
-        
-        return panel;
-    }
-
-    private void initializeSimulationNameButton() {
-        JButton simulationNameButton = createSimulationNameButton();
-        mainPanel.add(simulationNameButton);
-    }
-
-    private JButton createSimulationNameButton() {
-        JButton button = new JButton("Load Simulation");
-        button.setFocusable(false);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                JsonReader jsonReader = new JsonReader("./data/" + textField.getText() + ".json");
-                try {
-                    new MainWindow(jsonReader.read());
-                } catch (IOException e1) {
-                    System.out.println("That name doesn't exist!");
-                }
-            }
-        });
-        return button;
+    @Override
+    protected void buttonAction() {
+        JsonReader jsonReader = new JsonReader("./data/" + textFields.get(0).getText() + ".json");
+        try {
+            new MainWindow(jsonReader.read());
+        } catch (IOException e1) {
+            System.out.println("That name doesn't exist!");
+        }
     }
 }
