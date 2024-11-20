1dcad2ab29b18d551bb487ef19624dab0aa66534
wmostrenko <wmostrenko@gmail.com>
2024-11-19 18:50:23 -0800

    Refactored change name window

--- src/main/ui/ChangeNameWindow.java
+++ src/main/ui/ChangeNameWindow.java
@@ -7,74 +7,29 @@ import java.awt.event.ActionEvent;
 import javax.swing.*;
 
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class ChangeNameWindow {
-    private JFrame frame;
-    private JPanel mainPanel;
-    private Simulation simulation;
-    private JTextField textField;
+public class ChangeNameWindow extends TextAndButtonWindow {
 
-    private static int WHITESPACE = 7;
-
-    public ChangeNameWindow(Simulation simulation) {
-        this.simulation = simulation;
-        initializeFrame();
+public ChangeNameWindow(Simulation simulation) {
+        super("Change Name", 300, 100, simulation);
     }
 
-    public void initializeFrame() {
-        frame = new JFrame();
-        this.frame.setTitle("Change Name");
-        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setSize(300, 100);
-        this.frame.setLocationRelativeTo(null);
-        this.frame.setResizable(false);
-        initializeMainPanel();
-        this.frame.setVisible(true);
+    @Override
+    protected void initializeLabels() {
+        labels = new ArrayList<JLabel>();
+        labels.add(new JLabel("Name: "));
     }
 
-    private void initializeMainPanel() {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(0, 1, 10, 0));
-        mainPanel.setBackground(Color.WHITE);
-        initializeFieldPanels();
-        initializeChangeNameButton();
-        mainPanel.setBorder(BorderFactory.createMatteBorder(WHITESPACE, WHITESPACE, WHITESPACE, WHITESPACE, Color.WHITE));
-
-        frame.add(mainPanel);
+    @Override
+    protected void initializeTextFields() {
+        textFields = new ArrayList<JTextField>();
+        textFields.add(new JTextField(simulation.getName()));
     }
 
-    private void initializeFieldPanels() {
-        mainPanel.add(createPanel());
-    }
-
-    private JPanel createPanel() {
-        JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(1, 2));
-        panel.setBackground(Color.WHITE);
-
-        JLabel label = new JLabel("Name: ");
-        panel.add(label);
-
-        textField = new JTextField(simulation.getName());
-        panel.add(textField);
-        
-        return panel;
-    }
-
-    private void initializeChangeNameButton() {
-        JButton changeNameButton = createChangeNameButton();
-        mainPanel.add(changeNameButton);
-    }
-
-    private JButton createChangeNameButton() {
-        JButton button = new JButton("Change Name");
-        button.setFocusable(false);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                simulation.changeName(textField.getText());
-            }
-        });
-        return button;
+    @Override
+    protected void buttonAction() {
+        simulation.changeName(textFields.get(0).getText());
     }
 }
