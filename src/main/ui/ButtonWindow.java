78c9dfc755c8e9c756e4a7912f73e3a2f9404c30
wmostrenko <wmostrenko@gmail.com>
2024-11-19 19:25:19 -0800

    Added subpace button window and refactored change reference frame window

--- src/main/ui/ButtonWindow.java
+++ src/main/ui/ButtonWindow.java
@@ -1,5 +1,41 @@
 package ui;
 
-public class ButtonWindow {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+
+import model.Simulation;
+
+public abstract class ButtonWindow extends SubWindow {
+    
+    public ButtonWindow(String title, Integer width, Integer height, Simulation simulation) {
+        super(title, width, height, simulation);
+    }
+
+    protected void initializeButtons(Integer start) {
+        for (int i = start; i < simulation.getNumberOfObjects(); i++) {
+            mainPanel.add(createButton(i));
+        }
+    }
+
+    private JButton createButton(int i) {
+        JButton button;
+        if (i == 0) {
+            button = new JButton("Stationary Reference Frame ");
+        } else {
+            button = new JButton("Object " + i);
+        }
+        button.setFocusable(false);
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                buttonAction(i);
+            }
+        });
+        return button;
+    }
+
+    protected abstract void buttonAction(Integer i);
 
 }
--- src/main/ui/ChangeReferenceFrameWindow.java
+++ src/main/ui/ChangeReferenceFrameWindow.java
@@ -1,68 +1,20 @@
 package ui;
 
 import model.Simulation;
-import java.awt.Color;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import javax.swing.*;
 
-import java.awt.event.ActionListener;
-
-public class ChangeReferenceFrameWindow {
-    private JFrame frame;
-    private JPanel mainPanel;
-    private Simulation simulation;
-
-    private static int WHITESPACE = 7;
+public class ChangeReferenceFrameWindow extends ButtonWindow {
 
     public ChangeReferenceFrameWindow(Simulation simulation) {
-        this.simulation = simulation;
-        initializeFrame();
+        super("Change Reference Frame", 300, 200, simulation);
     }
 
-    public void initializeFrame() {
-        frame = new JFrame();
-        this.frame.setTitle("Change Reference Frame");
-        this.frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        this.frame.setSize(300, 200);
-        this.frame.setLocationRelativeTo(null);
-        this.frame.setResizable(false);
-        initializeMainPanel();
-        this.frame.setVisible(true);
+    @Override
+    protected void initializeElements() {
+        initializeButtons(0);
     }
 
-    private void initializeMainPanel() {
-        mainPanel = new JPanel();
-        mainPanel.setLayout(new GridLayout(0, 1, 10, 0));
-        mainPanel.setBackground(Color.WHITE);
-        initializeObjectButtons();
-        mainPanel.setBorder(BorderFactory.createMatteBorder(WHITESPACE, WHITESPACE, WHITESPACE, WHITESPACE, Color.WHITE));
-
-        frame.add(mainPanel);
-    }
-
-    private void initializeObjectButtons() {
-        for (int i = 0; i < simulation.getNumberOfObjects(); i++) {
-            mainPanel.add(createButton(i));
-        }
-    }
-
-    private JButton createButton(int index) {
-        JButton button;
-
-        if (index == 0) {
-            button = new JButton("Stationary Reference Frame ");
-        } else {
-            button = new JButton("Object " + index);
-        }
-
-        button.setFocusable(false);
-        button.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                simulation.setCurrentReferenceFrame(simulation.getObjectAt(index));
-            }
-        });
-        return button;
+    @Override
+    protected void buttonAction(Integer i) {
+        simulation.setCurrentReferenceFrame(simulation.getObjectAt(i));
     }
 }
